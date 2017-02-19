3a75ed98b92a3ac516b8e58e42e1debca2a46f5b
Yaroslav Yermilov <yermilov17@gmail.com>
2017-02-19 22:53:00 +0200

    update basic configuration

--- SiteConfig.groovy
+++ SiteConfig.groovy
@@ -2,14 +2,14 @@ import com.sysgears.octopress.deploy.GHPagesDeployer
 import com.sysgears.octopress.mapping.ResourceMapper
 import com.sysgears.octopress.taglibs.OctopressTagLib
 
+
 /*
  * Grain configuration.
  */
 
-// Resource mapper and tag libs.
 resource_mapper = new ResourceMapper(site).map
-tag_libs = [OctopressTagLib]
-non_script_files = [/(?i).*\.(js)$/]
+tag_libs = [ OctopressTagLib ]
+non_script_files = [ /(?i).*\.(js)$/ ]
 
 features {
     highlight = 'pygments' // 'none', 'pygments'
@@ -24,13 +24,13 @@ environments {
     }
     prod {
         log.info 'Production environment is used'
-        url = '' // the site URL, for instance http://example.com
+        url = 'https://yermilov.github.io'
         show_unpublished = false
         features {
             minify_xml = true
             minify_html = true
             minify_js = true
-            minify_css = false
+            minify_css = true
         }
     }
     cmd {
@@ -53,16 +53,19 @@ ruby {
     //ruby_gems = '2.2.2'
 }
 
-// Deployment settings.
-s3_bucket = 'www.example.com'
-s3_deploy_cmd = "s3cmd sync --acl-public --reduced-redundancy ${destination_dir}/ s3://${s3_bucket}/"
+/*
+ * Deployment configuration.
+ */
 
-rsync_ssh_user = 'user@example.com'
-rsync_ssh_port = '22'
-rsync_document_root = '~/public_html/'
-rsync_deploy_cmd = "rsync -avze 'ssh -p ${rsync_ssh_port}' --delete ${destination_dir} ${rsync_ssh_user}:${rsync_document_root}"
+// s3_bucket = 'www.example.com'
+// s3_deploy_cmd = "s3cmd sync --acl-public --reduced-redundancy ${destination_dir}/ s3://${s3_bucket}/"
 
-gh_pages_url = '' // path to GitHub repository in format git@github.com:{username}/{repo}.git
+// rsync_ssh_user = 'user@example.com'
+// rsync_ssh_port = '22'
+// rsync_document_root = '~/public_html/'
+// rsync_deploy_cmd = "rsync -avze 'ssh -p ${rsync_ssh_port}' --delete ${destination_dir} ${rsync_ssh_user}:${rsync_document_root}"
+
+gh_pages_url = 'git@github.com:yermilov/yermilov.github.io.git'
 github_pages_deploy_cmd = new GHPagesDeployer(site).deploy
 
 deploy = github_pages_deploy_cmd
@@ -71,13 +74,12 @@ deploy = github_pages_deploy_cmd
  * Site configuration.
  */
 
-// General settings.
-title = 'Octopress theme for Grain' // blog name for the header, title and RSS feed
-subtitle = 'Grain is a static web site building framework for Groovy' // blog brief description for the header
-author = 'SysGears'                 // author name for Copyright, Metadata and RSS feed
-meta_description = ''               // blog description for Metadata
+title = 'Development notes'
+subtitle = 'by Yaroslav Yermilov'
+author = 'Yaroslav Yermilov'
+meta_description = 'development notes by Yaroslav Yermilov'
 
-// Blog and Archive.
+// Blog and Archive
 posts_per_blog_page = 5             // the number of posts to display per blog page
 posts_per_archive_page = 10         // the number of posts to display per archive page
 disqus {
@@ -98,7 +100,7 @@ enable_site_search = true           // defines whether to enable site search
 subscribe_url = ''                  // url to subscribe by email (service integration required)
 
 // Google Analytics.
-google_analytics_tracking_id = ''   // google analytics tracking code, for details visit: http://www.google.com/analytics/
+google_analytics_tracking_id = 'UA-91383043-1'
 
 // Sharing.
 sharing {
@@ -126,9 +128,19 @@ sharing {
 }
 
 // Sidebar modules that should be included by default.
-default_asides = ['asides/recent_posts.html', 'asides/bitbucket.html', 'asides/github.html', 'asides/tweets.html', 'asides/delicious.html',
-        'asides/pinboard.html', 'asides/about.html', 'asides/facebook.html', 'asides/twitter.html',
-        'asides/instagram.html', 'asides/google_plus.html']
+default_asides = [
+        'asides/recent_posts.html',
+//        'asides/bitbucket.html',
+        'asides/github.html',
+//        'asides/tweets.html',
+//        'asides/delicious.html',
+//        'asides/pinboard.html',
+//        'asides/about.html',
+        'asides/facebook.html',
+        'asides/twitter.html',
+//        'asides/instagram.html',
+//        'asides/google_plus.html'
+]
 
 asides {
 
@@ -157,7 +169,7 @@ asides {
 
     // GitHub repositories.
     github {
-        user = 'sysgears'           // GitHub (https://github.com/) username
+        user = 'yermilov'           // GitHub (https://github.com/) username
         show_profile_link = true    // whether to show link to GitHub profile
         skip_forks = true
         count = 10                  // the number of repositories to show
@@ -165,7 +177,7 @@ asides {
 
     // The latest tweets.
     tweets {
-        user = 'sysgears'           // Twitter (https://twitter.com/) username
+        user = 'yermilov17'           // Twitter (https://twitter.com/) username
         count = 2                   // the number of tweets to display
         //consumer_key = ''         // to get consumer key and secret go to https://dev.twitter.com/apps and create a new application
         //consumer_secret = ''
@@ -181,20 +193,19 @@ asides {
 
     // Links to social networks:
     google_plus {
-        user = '109746189379932479469' // Google plus (https://plus.google.com/) user id
+        user = '105565521760571946004'
     }
     twitter {
-        user = 'sysgears'           // Twitter (https://twitter.com/) username
+        user = 'yermilov17'
     }
     facebook {
-        user = 'sysgears'           // Facebook (https://www.facebook.com/) username
+        user = 'yaroslav.yermilov'
     }
     instagram {
-        user = ''                   // Instagram (http://instagram.com/) username
+        user = 'yaroslav.yermilov'
     }
 
-    // Blog owner description.
-    about_author = 'A brief description of blog owner.'
+    about_author = '?'
 }
 
 commands = [
