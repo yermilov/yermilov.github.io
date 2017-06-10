7d7f23cbfd94a88683a3d445c4eeed641201474e
Travis-CI <yermilov17@gmail.com>
2017-06-10 16:04:35 +0000

    deployed from build-96 by Travis-CI from 1d42db00a29201025275c869d51dc0ff3d3b146c (#53. temporary remove spock article)

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/home/travis/build/yermilov/yermilov.github.io/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/home/travis/build/yermilov/yermilov.github.io/theme/sass"
+generated_images_path = "/home/travis/build/yermilov/yermilov.github.io/.cache/compass/images"
+images_path = "/home/travis/build/yermilov/yermilov.github.io/theme/images"
+fonts_path = "/home/travis/build/yermilov/yermilov.github.io/theme/fonts"
+
+line_comments = false
+output_style = :compressed
